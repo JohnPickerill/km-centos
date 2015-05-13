3c15f19eba5ef1247129e0bdd96f8f7a0a0e599d
John Pickerill <git@curiouscrab.com>
2015-05-13 15:14:30 +0100

    sdjkfhjk

--- makeindex.bat
+++ makeindex.bat
@@ -1,25 +1,25 @@
-curl -XPUT 'localhost:9203/knowledge_manage' -d '{
+curl -XPUT 'localhost:9200/knowledge_manage' -d '{
     "mappings":{
         "information":{
             "properties":{
-                "id": {"type":"string"},
-                "title": {"type":"string", "index":"analyzed"},
-                "scope": {"type":"string", "index":"analyzed"},
+                "id": {"type":"string", "index":"not_analyzed"},
+                "title": {"type":"string", "index":"analyzed", "analyzer" : "english"},
+                "scope": {"type":"string", "index":"analyzed", "analyzer" : "english"},
                 "type": {"type":"string"},
-                "items": {"properties" : {"item": {"type":"string"},"type": {"type":"string"}}},
+                "items": {"properties" : {"item": {"type":"string", "index":"not_analyzed"},"type": {"type":"string", "index":"not_analyzed"}}},
                 "lastupdate": {"type" : "date","format" : "date_time"},
                 "popularity": {"type":"integer"},
-                "cluster": {"type":"string"},
-                "master": {"properties":{"where":{"type":"string"}, "filename":{"type":"string"}}},
+                "cluster": {"type":"string","index":"not_analyzed"},
+                "master": {"properties":{"where":{"type":"string", "index":"no"}, "filename":{"type":"string", "index":"no"}}},
                 "keywords": {"type":"string", "index_name":"keyword","analyzer":"keyword"},
                 "facets": {"type":"nested",
                            "properties":{"name": {"type":"string", "index":"not_analyzed"},"foci":{"type":"string","index_name":"focus","index":"not_analyzed"}}},
                 "kmlinks": {
-                    "properties" : {"id": {"type":"string"}}},
+                    "properties" : {"id": {"type":"string","index":"not_analyzed"}}},
                 "extlinks": {
-                    "properties" : {"name": {"type":"string"},"url":{"type":"string"},"scope": {"type":"string"}}},
-                "content": {"type":"string", "index":"not_analyzed"},
-                "markup": {"type":"string", "index":"not_analyzed"}
+                    "properties" : {"name": {"type":"string","index":"not_analyzed"},"url":{"type":"string","index":"not_analyzed"},"scope": {"type":"string","index":"no"}}},
+                "content": {"type":"string", "index":"no"},
+                "markup": {"type":"string", "index":"no"}
             }
         }
     }
